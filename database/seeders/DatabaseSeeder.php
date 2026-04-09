4ac10de848114495eeb8fef74a1bc48faa7b7e09
Ahmed901aa <ahmed.ly2025@gmail.com>
2026-04-09 22:21:59 +0200

    save

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\AppSetting;
 use App\Models\Banner;
 use App\Models\FuelPrice;
+use App\Models\ProviderProfile;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
